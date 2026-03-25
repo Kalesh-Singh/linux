369cc7a7efd44f82c292db1b80e4733a33f698f7
Kalesh Singh <kaleshsingh96@gmail.com>
2026-03-25 17:58:18 +0000

    Fix reuse of MAP_HUGETLB flag

--- include/linux/ptshare.h
+++ include/linux/ptshare.h
@@ -2,6 +2,7 @@
 #define _LINUX_PTSHARE_H
 
 #include <linux/mm_types.h>
+#include <linux/mman.h>
 #include <linux/mm.h>
 
 /* TODO: Remove */
@@ -12,6 +13,11 @@ extern struct mm_struct *ptshare_mm;
 static inline bool vma_shares_pagetables(const struct vm_area_struct *vma)
 {
 	return vma->vm_flags & VM_PT_SHARED;
+}
+
+static inline bool has_map_hugetlb_flag(unsigned long flags)
+{
+	return flags & MAP_HUGETLB && !IS_ENABLED(CONFIG_PTSHARE);
 }
 
 /**
