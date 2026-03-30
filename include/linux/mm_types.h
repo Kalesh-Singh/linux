135ef315d8f4f7099beb7a834d44897ab4d65b1e
Kalesh Singh <kaleshsingh96@gmail.com>
2026-03-30 17:03:55 +0000

    mm/ptshare: Add vm_ptshare union to vm_area_struct
    To support per-domain refcounting and descriptor tracking, we add a
    union to vm_area_struct to accommodate different roles of VMAs in
    the ptshare subsystem:
    
     - __vm_ptshare_desc: For client VMAs (those sharing page tables),
       this points to the associated ptshare_desc representing the
       sharing domain.
     - __vm_ptshare_refcount: For shadow VMAs (those hosting the shared
       page tables in the headless shadow MM), this stores the active
       count of client processes mapping this range.
    
    The use of a union ensures that the VMA structure size remains
    optimized while providing the necessary metadata for both client
    and shadow roles.
    
    Fields are prefixed with '__' to indicate they should not be
    accessed directly. Accessor and setter functions are provided in
    ptshare.h to ensure correct usage based on the VMA's role and to
    properly encapsulate the internal representation:
    
     - vma_ptshare_desc(): Getter for the sharing domain descriptor.
     - vma_set_ptshare_desc(): Setter for the sharing domain descriptor.
     - vma_ptshare_refcount(): Accessor for the client refcount.
    
    Update vm_area_init_from() to correctly initialize the ptshare
    descriptor for client VMAs during VMA duplication (fork())
     using the provided accessors.
    
    Signed-off-by: Kalesh Singh <kaleshsingh96@gmail.com>

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -1056,6 +1056,13 @@ struct vm_area_struct {
 #ifdef __HAVE_PFNMAP_TRACKING
 	struct pfnmap_track_ctx *pfnmap_track_ctx;
 #endif
+#ifdef CONFIG_PTSHARE
+	union {
+		/* For client VMAs: pointer to domain */
+		struct ptshare_desc *__vm_ptshare_desc;
+		refcount_t __vm_ptshare_refcount;  /* For shadow VMAs: number of clients */
+	};
+#endif
 } __randomize_layout;
 
 /* Clears all bits in the VMA flags bitmap, non-atomically. */
