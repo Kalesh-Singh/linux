12e84e5daaac26fdfc427faed2b99b389b452137
Kalesh Singh <kaleshsingh@google.com>
2026-06-23 02:50:16 +0000

    mm: Scale madvise page range adjustments to process page size
    Under CONFIG_ARM64_PER_PROCESS_PAGE_SIZE, processes run with a
    process-specific translation granule size. This requires that range
    alignments, batch checks, PTE walks, and TLB page size updates in
    madvise be evaluated dynamically rather than relying on static host
    PAGE_SIZE constants.
    
    Update madvise range alignment checks and length rounding inside
    mm/madvise.c to use MM_PAGE_ALIGNED() and MM_PAGE_ALIGN() macros.
    Scale swapin_walk_pmd_entry(), madvise_folio_pte_batch(), and regular
    PTE loops to increment and step through ranges dynamically using
    MM_PAGE_SIZE() and MM_PHYS_PFN(). Include <linux/ppps.h> to resolve
    the dynamic paging wrappers.
    
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

--- mm/madvise.c
+++ mm/madvise.c
@@ -18,6 +18,7 @@
 #include <linux/fadvise.h>
 #include <linux/sched.h>
 #include <linux/sched/mm.h>
+#include <linux/ppps.h>
 #include <linux/mm_inline.h>
 #include <linux/mmu_context.h>
 #include <linux/string.h>
@@ -193,7 +194,7 @@ static int swapin_walk_pmd_entry(pmd_t *pmd, unsigned long start,
 	spinlock_t *ptl;
 	unsigned long addr;
 
-	for (addr = start; addr < end; addr += PAGE_SIZE) {
+	for (addr = start; addr < end; addr += MM_PAGE_SIZE(vma->vm_mm)) {
 		pte_t pte;
 		softleaf_t entry;
 		struct folio *folio;
@@ -341,11 +342,12 @@ static inline bool can_do_file_pageout(struct vm_area_struct *vma)
 	       file_permission(vma->vm_file, MAY_WRITE) == 0;
 }
 
-static inline int madvise_folio_pte_batch(unsigned long addr, unsigned long end,
+static inline int madvise_folio_pte_batch(struct mm_struct *mm,
+					  unsigned long addr, unsigned long end,
 					  struct folio *folio, pte_t *ptep,
 					  pte_t *ptentp)
 {
-	int max_nr = (end - addr) / PAGE_SIZE;
+	int max_nr = (end - addr) / MM_PAGE_SIZE(mm);
 
 	return folio_pte_batch_flags(folio, NULL, ptep, ptentp, max_nr,
 				     FPB_MERGE_YOUNG_DIRTY);
@@ -447,14 +449,14 @@ huge_unlock:
 
 regular_folio:
 #endif
-	tlb_change_page_size(tlb, PAGE_SIZE);
+	tlb_change_page_size(tlb, MM_PAGE_SIZE(mm));
 restart:
 	start_pte = pte = pte_offset_map_lock(vma->vm_mm, pmd, addr, &ptl);
 	if (!start_pte)
 		return 0;
 	flush_tlb_batched_pending(mm);
 	lazy_mmu_mode_enable();
-	for (; addr < end; pte += nr, addr += nr * PAGE_SIZE) {
+	for (; addr < end; pte += nr, addr += nr * MM_PAGE_SIZE(mm)) {
 		nr = 1;
 		ptent = ptep_get(pte);
 
@@ -486,7 +488,7 @@ restart:
 		 * next pte in the range.
 		 */
 		if (folio_test_large(folio)) {
-			nr = madvise_folio_pte_batch(addr, end, folio, pte, &ptent);
+			nr = madvise_folio_pte_batch(mm, addr, end, folio, pte, &ptent);
 			if (nr < folio_nr_pages(folio)) {
 				int err;
 
@@ -670,13 +672,13 @@ static int madvise_free_pte_range(pmd_t *pmd, unsigned long addr,
 		if (madvise_free_huge_pmd(tlb, vma, pmd, addr, next))
 			return 0;
 
-	tlb_change_page_size(tlb, PAGE_SIZE);
+	tlb_change_page_size(tlb, MM_PAGE_SIZE(mm));
 	start_pte = pte = pte_offset_map_lock(mm, pmd, addr, &ptl);
 	if (!start_pte)
 		return 0;
 	flush_tlb_batched_pending(mm);
 	lazy_mmu_mode_enable();
-	for (; addr != end; pte += nr, addr += PAGE_SIZE * nr) {
+	for (; addr != end; pte += nr, addr += MM_PAGE_SIZE(mm) * nr) {
 		nr = 1;
 		ptent = ptep_get(pte);
 
@@ -691,7 +693,7 @@ static int madvise_free_pte_range(pmd_t *pmd, unsigned long addr,
 			softleaf_t entry = softleaf_from_pte(ptent);
 
 			if (softleaf_is_swap(entry)) {
-				max_nr = (end - addr) / PAGE_SIZE;
+				max_nr = (end - addr) / MM_PAGE_SIZE(mm);
 				nr = swap_pte_batch(pte, max_nr, ptent);
 				nr_swap -= nr;
 				swap_put_entries_direct(entry, nr);
@@ -715,7 +717,7 @@ static int madvise_free_pte_range(pmd_t *pmd, unsigned long addr,
 		 * next pte in the range.
 		 */
 		if (folio_test_large(folio)) {
-			nr = madvise_folio_pte_batch(addr, end, folio, pte, &ptent);
+			nr = madvise_folio_pte_batch(mm, addr, end, folio, pte, &ptent);
 			if (nr < folio_nr_pages(folio)) {
 				int err;
 
@@ -989,7 +991,7 @@ static long madvise_populate(struct madvise_behavior *madv_behavior)
 				return -ENOMEM;
 			}
 		}
-		start += pages * PAGE_SIZE;
+		start += pages * MM_PAGE_SIZE(mm);
 	}
 	return 0;
 }
@@ -1183,7 +1185,7 @@ static long madvise_guard_install(struct madvise_behavior *madv_behavior)
 
 		if (err == 0) {
 			unsigned long nr_expected_pages =
-				PHYS_PFN(range->end - range->start);
+				MM_PHYS_PFN(vma->vm_mm, range->end - range->start);
 
 			VM_WARN_ON(nr_pages != nr_expected_pages);
 			return 0;
@@ -1841,9 +1843,9 @@ static bool is_valid_madvise(unsigned long start, size_t len_in, int behavior)
 	if (!madvise_behavior_valid(behavior))
 		return false;
 
-	if (!PAGE_ALIGNED(start))
+	if (!MM_PAGE_ALIGNED(start))
 		return false;
-	len = PAGE_ALIGN(len_in);
+	len = MM_PAGE_ALIGN(len_in);
 
 	/* Check to see whether len was rounded up from small -ve to zero */
 	if (len_in && !len)
@@ -1873,7 +1875,7 @@ static bool madvise_should_skip(unsigned long start, size_t len_in,
 		*err = -EINVAL;
 		return true;
 	}
-	if (start + PAGE_ALIGN(len_in) == start) {
+	if (start + MM_PAGE_ALIGN(len_in) == start) {
 		*err = 0;
 		return true;
 	}
@@ -1920,7 +1922,7 @@ static int madvise_do_behavior(unsigned long start, size_t len_in,
 	}
 
 	range->start = get_untagged_addr(madv_behavior->mm, start);
-	range->end = range->start + PAGE_ALIGN(len_in);
+	range->end = range->start + MM_PAGE_ALIGN(madv_behavior->mm, len_in);
 
 	blk_start_plug(&plug);
 	if (is_madvise_populate(madv_behavior))
