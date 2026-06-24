cdfcda6767e403d1fc568c2596a459781e8d77a9
Kalesh Singh <kaleshsingh@google.com>
2026-06-24 10:58:53 +0000

    mm: Scale copy_vma and remap movements
    Under CONFIG_ARM64_PER_PROCESS_PAGE_SIZE, virtual memory range copies
    and remappping movements must calculate and propagate process-specific
    subpage slice offsets. This ensures that the target remapped VMAs
    maintain alignment with page cache boundaries.
    
    Extend the copy_vma() signature inside mm/vma.h and mm/vma.c to
    accept a slice_off parameter. During VMA allocation or merging within
    copy_vma(), write this slice offset onto the target VMA structure.
    Initialize the slice offset using ppps_map_slice_off() for newly
    duplicated, unfaulted anonymous VMAs.
    
    Update copy_vma_and_data() inside mm/mremap.c to calculate the target
    range's starting page offset and slice offset dynamically by calling
    vma_pgoff_offset() and vma_slice_offset(), and pass the resulting
    slice offset to copy_vma(). Update VMA user-space unit tests inside
    tools/testing/vma/tests/vma.c to conform to the modified copy_vma()
    signature.
    
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

--- mm/vma.h
+++ mm/vma.h
@@ -487,7 +487,7 @@ void unlink_file_vma_batch_add(struct unlink_vma_file_batch *vb,
 
 struct vm_area_struct *copy_vma(struct vm_area_struct **vmap,
 	unsigned long addr, unsigned long len, pgoff_t pgoff,
-	bool *need_rmap_locks);
+	unsigned int slice_off, bool *need_rmap_locks);
 
 struct anon_vma *find_mergeable_anon_vma(struct vm_area_struct *vma);
 
