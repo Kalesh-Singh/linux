c75f37a2f9333eb1705d9ad244cf45c2b9b71b68
Kalesh Singh <kaleshsingh@google.com>
2026-06-26 23:53:47 +0000

    mm/ppps: Remove redundant vma_nr_slices and rename vmg_nr_slices
    Since vma_pages() has already been redefined to return dynamic
    process-specific pages (using MM_PAGE_SHIFT), vma_nr_slices() in
    mm/ppps.h is redundant. Remove it and update callers to use
    vma_pages(). Similarly, rename vmg_nr_slices() to vmg_pages()
    for consistency.
    
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

--- mm/vma.c
+++ mm/vma.c
@@ -1422,7 +1422,7 @@ static int vms_gather_munmap_vmas(struct vma_munmap_struct *vms,
 			goto munmap_gather_failed;
 
 		vma_mark_detached(next);
-		nrpages = vma_nr_slices(next);
+		nrpages = vma_pages(next);
 
 		vms->nr_pages += nrpages;
 		if (vma_test(next, VMA_LOCKED_BIT))
@@ -3264,7 +3264,7 @@ int __vm_munmap(unsigned long start, size_t len, bool unlock)
  */
 int insert_vm_struct(struct mm_struct *mm, struct vm_area_struct *vma)
 {
-	unsigned long charged = vma_nr_slices(vma);
+	unsigned long charged = vma_pages(vma);
 
 	if (find_vma_intersection(mm, vma->vm_start, vma->vm_end))
 		return -ENOMEM;
