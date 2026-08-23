e68bb4c3bdcefc1e49b6631b05d9d0ff90cdd7f1
Kalesh Singh <kaleshsingh@google.com>
2026-08-23 09:55:46 +0000

    mm/userfaultfd: use mm_pte_*() inlined helpers in validate_range()
    In validate_range(), the start address alignment is validated against
    the global PAGE_MASK macro.
    
    Adopt mm_pte_aligned() in validate_range().

--- mm/userfaultfd.c
+++ mm/userfaultfd.c
@@ -3561,7 +3561,7 @@ static __always_inline int validate_unaligned_range(
 static __always_inline int validate_range(struct mm_struct *mm,
 					  __u64 start, __u64 len)
 {
-	if (start & ~PAGE_MASK)
+	if (!mm_pte_aligned(mm, start))
 		return -EINVAL;
 
 	return validate_unaligned_range(mm, start, len);
