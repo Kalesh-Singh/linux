a5edba658b25eb731f7ff9e2e30be771841689bb
Kalesh Singh <kaleshsingh@google.com>
2026-08-23 09:42:42 +0000

    fs/aio: use mm_pte_size() in aio_setup_ring()
    In aio_setup_ring(), the event ring buffer sizing and mmap area
    calculations use global PAGE_SIZE.
    
    Adopt mm_pte_size() to reference the target mm page geometry directly.

--- fs/aio.c
+++ fs/aio.c
@@ -559,7 +559,7 @@ static int aio_setup_ring(struct kioctx *ctx, unsigned int nr_events)
 	}
 
 	ctx->aio_ring_file = file;
-	nr_events = (PAGE_SIZE * nr_pages - sizeof(struct aio_ring))
+	nr_events = (mm_pte_size(mm) * nr_pages - sizeof(struct aio_ring))
 			/ sizeof(struct io_event);
 
 	ctx->ring_folios = ctx->internal_folios;
@@ -593,7 +593,7 @@ static int aio_setup_ring(struct kioctx *ctx, unsigned int nr_events)
 		return -ENOMEM;
 	}
 
-	ctx->mmap_size = nr_pages * PAGE_SIZE;
+	ctx->mmap_size = nr_pages * mm_pte_size(mm);
 	pr_debug("attempting mmap of %lu bytes\n", ctx->mmap_size);
 
 	if (mmap_write_lock_killable(mm)) {
