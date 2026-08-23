419e53d6aeed292472aa9e8202bcdf59748268b7
Kalesh Singh <kaleshsingh@google.com>
2026-08-23 10:57:53 +0000

    mm/mapping_dirty_helpers: use mm_pte_size() and vma_file_offset()
    In clean_record_pte(), address space offset arithmetic is open-coded
    using PAGE_SHIFT. In addition, wp_pte() and clean_record_pte() bound
    TLB flush ranges using PAGE_SIZE.
    
    Adopt vma_file_offset() for clean pgoff calculations and mm_pte_size()
    for TLB flush range boundaries.

--- mm/mapping_dirty_helpers.c
+++ mm/mapping_dirty_helpers.c
@@ -45,7 +45,7 @@ static int wp_pte(pte_t *pte, unsigned long addr, unsigned long end,
 		wpwalk->total++;
 		wpwalk->tlbflush_start = min(wpwalk->tlbflush_start, addr);
 		wpwalk->tlbflush_end = max(wpwalk->tlbflush_end,
-					   addr + PAGE_SIZE);
+					   addr + mm_pte_size(walk->mm));
 	}
 
 	return 0;
@@ -94,8 +94,8 @@ static int clean_record_pte(pte_t *pte, unsigned long addr,
 	pte_t ptent = ptep_get(pte);
 
 	if (pte_dirty(ptent)) {
-		pgoff_t pgoff = ((addr - walk->vma->vm_start) >> PAGE_SHIFT) +
-			walk->vma->vm_pgoff - cwalk->bitmap_pgoff;
+		pgoff_t pgoff = vma_file_offset(walk->vma, addr) -
+			cwalk->bitmap_pgoff;
 		pte_t old_pte = ptep_modify_prot_start(walk->vma, addr, pte);
 
 		ptent = pte_mkclean(old_pte);
@@ -104,7 +104,7 @@ static int clean_record_pte(pte_t *pte, unsigned long addr,
 		wpwalk->total++;
 		wpwalk->tlbflush_start = min(wpwalk->tlbflush_start, addr);
 		wpwalk->tlbflush_end = max(wpwalk->tlbflush_end,
-					   addr + PAGE_SIZE);
+					   addr + mm_pte_size(walk->mm));
 
 		__set_bit(pgoff, cwalk->bitmap);
 		cwalk->start = min(cwalk->start, pgoff);
