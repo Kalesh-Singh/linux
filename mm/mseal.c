f9d70c23700ee193c44dc1a59da3bf98132f5442
Kalesh Singh <kaleshsingh@google.com>
2026-08-23 09:53:29 +0000

    mm/mseal: use mm_pte_*() inlined helpers in do_mseal()
    In mm/mseal.c, start address alignment checking and length rounding
    directly reference global PAGE_ALIGNED and PAGE_ALIGN macros.
    
    Adopt mm_pte_aligned() and mm_pte_align() in do_mseal().

--- mm/mseal.c
+++ mm/mseal.c
@@ -152,10 +152,10 @@ int do_mseal(unsigned long start, size_t len_in, unsigned long flags)
 		return -EINVAL;
 
 	start = untagged_addr(start);
-	if (!PAGE_ALIGNED(start))
+	if (!mm_pte_aligned(mm, start))
 		return -EINVAL;
 
-	len = PAGE_ALIGN(len_in);
+	len = mm_pte_align(mm, len_in);
 	/* Check to see whether len was rounded up from small -ve to zero. */
 	if (len_in && !len)
 		return -EINVAL;
