50bb4506fe28216e70f656a9587fe586fba509fd
Kalesh Singh <kaleshsingh@google.com>
2026-08-25 05:31:43 +0000

    mm/vma: scale unmapping alignment and boundaries
    Scale unmapping alignment checks in do_vmi_munmap() to use mm_offset_in_pte()
    and mm_pte_align() instead of hardcoded compile-time offset_in_page() and
    PAGE_ALIGN().

--- mm/vma.c
+++ mm/vma.c
@@ -1634,10 +1634,10 @@ int do_vmi_munmap(struct vma_iterator *vmi, struct mm_struct *mm,
 	unsigned long end;
 	struct vm_area_struct *vma;
 
-	if ((offset_in_page(start)) || start > TASK_SIZE || len > TASK_SIZE-start)
+	if ((mm_offset_in_pte(mm, start)) || start > TASK_SIZE || len > TASK_SIZE-start)
 		return -EINVAL;
 
-	end = start + PAGE_ALIGN(len);
+	end = start + mm_pte_align(mm, len);
 	if (end == start)
 		return -EINVAL;
 
