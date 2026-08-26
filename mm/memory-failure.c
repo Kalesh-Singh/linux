2a63e3d082e6cf8519716756ff879e37ff5de69b
Kalesh Singh <kaleshsingh@google.com>
2026-08-26 09:17:09 +0000

    mm/memory-failure: reset to state at master

--- mm/memory-failure.c
+++ mm/memory-failure.c
@@ -727,28 +727,36 @@ static int check_hwpoisoned_pmd_entry(pmd_t *pmdp, unsigned long addr,
 }
 #endif
 
-static int hwpoison_pmd_entry(pmd_t *pmdp, unsigned long addr,
-			      unsigned long next, struct mm_walk *walk)
+static int hwpoison_pte_range(pmd_t *pmdp, unsigned long addr,
+			      unsigned long end, struct mm_walk *walk)
 {
 	struct hwpoison_walk *hwp = walk->private;
-	spinlock_t *ptl = pmd_trans_huge_lock(pmdp, walk->vma);
 	int ret = 0;
+	pte_t *ptep, *mapped_pte;
+	spinlock_t *ptl;
 
+	ptl = pmd_trans_huge_lock(pmdp, walk->vma);
 	if (ptl) {
 		ret = check_hwpoisoned_pmd_entry(pmdp, addr, hwp);
 		spin_unlock(ptl);
-		walk->action = ACTION_CONTINUE;
+		goto out;
 	}
+
+	mapped_pte = ptep = pte_offset_map_lock(walk->vma->vm_mm, pmdp,
+						addr, &ptl);
+	if (!ptep)
+		goto out;
+
+	for (; addr != end; ptep++, addr += PAGE_SIZE) {
+		ret = check_hwpoisoned_entry(ptep_get(ptep), addr, PAGE_SHIFT,
+					     hwp->pfn, &hwp->tk);
+		if (ret == 1)
+			break;
+	}
+	pte_unmap_unlock(mapped_pte, ptl);
+out:
+	cond_resched();
 	return ret;
-}
-
-static int hwpoison_pte_entry(pte_t *ptep, unsigned long addr,
-			      unsigned long next, struct mm_walk *walk)
-{
-	struct hwpoison_walk *hwp = walk->private;
-
-	return check_hwpoisoned_entry(ptep_get(ptep), addr, PAGE_SHIFT,
-				      hwp->pfn, &hwp->tk);
 }
 
 #ifdef CONFIG_HUGETLB_PAGE
@@ -781,8 +789,7 @@ static int hwpoison_test_walk(unsigned long start, unsigned long end,
 }
 
 static const struct mm_walk_ops hwpoison_walk_ops = {
-	.pmd_entry = hwpoison_pmd_entry,
-	.pte_entry = hwpoison_pte_entry,
+	.pmd_entry = hwpoison_pte_range,
 	.hugetlb_entry = hwpoison_hugetlb_range,
 	.test_walk = hwpoison_test_walk,
 	.walk_lock = PGWALK_RDLOCK,
