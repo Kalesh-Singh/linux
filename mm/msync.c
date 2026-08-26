84dc586e7d82d8ef311b161f68a1289dff2d91bc
Kalesh Singh <kaleshsingh@google.com>
2026-08-26 07:57:26 +0000

    mm/msync: reset to state at master
    Reset mm/msync.c to the state at local master before applying the P3S
    shadow context pattern.

--- mm/msync.c
+++ mm/msync.c
@@ -41,12 +41,12 @@ SYSCALL_DEFINE3(msync, unsigned long, start, size_t, len, int, flags)
 
 	if (flags & ~(MS_ASYNC | MS_INVALIDATE | MS_SYNC))
 		goto out;
-	if (!mm_pte_aligned(mm, start))
+	if (offset_in_page(start))
 		goto out;
 	if ((flags & MS_ASYNC) && (flags & MS_SYNC))
 		goto out;
 	error = -ENOMEM;
-	len = mm_pte_align(mm, len);
+	len = (len + ~PAGE_MASK) & PAGE_MASK;
 	end = start + len;
 	if (end < start)
 		goto out;
@@ -85,7 +85,8 @@ SYSCALL_DEFINE3(msync, unsigned long, start, size_t, len, int, flags)
 			goto out_unlock;
 		}
 		file = vma->vm_file;
-		fstart = (start - vma->vm_start) + vma_file_offset(vma);
+		fstart = (start - vma->vm_start) +
+			 ((loff_t)vma->vm_pgoff << PAGE_SHIFT);
 		fend = fstart + (min(end, vma->vm_end) - start) - 1;
 		start = vma->vm_end;
 		if ((flags & MS_SYNC) && file &&
